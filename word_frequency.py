d1cd78b7e1d4eff7a0bc9b0e87ef06825fb34343
Jordyn Price <88625349+JordynAntonia@users.noreply.github.com>
2021-10-27 02:13:21 -0400

    commit

--- word_frequency.py
+++ word_frequency.py
@@ -5,11 +5,11 @@ STOP_WORDS = [
 ]
 
 
-def print_word_freq(file):
+def clean_text(line):
     """Read in `file` and print out the frequency of words in that file."""
     
     # removes the punctuation from the text
-    with open("praise_song_for_the_day.txt") as praise_song_for_the_day:
+    #calling function to clean text within the file, opening file, removing punctuation and then closing the file. Looking at each line for punctuation errors with open("praise_song_for_the_day.txt") as praise_song_for_the_day:
         lines = praise_song_for_the_day.readlines()
         print(f"{len(lines)} lines in the file.")
         for line in lines:
@@ -23,17 +23,27 @@ def print_word_freq(file):
             
             
 
-    #normalize ALL words within each string to lowercase via "geeksforgeeks.org"
+    #normalize ALL words within each string to lowercase via "geeksforgeeks.org", "docs.python.org","bytes.lower()"
+def clean_text(text):
     with open("praise_song_for_the_day.txt") as praise_song_for_the_day:
-    string = ""
+    text = praise_song_for_the_day.readlines()
+    print(b"{len(lines)} lines in the file.")
     lower_string = string.lower()
     text = text.lower()
     all_letters = "abcdefghijklmnopqrstuvwxyz"
-    text_to_keep =""
-    print(lower_string)
+    text_to_keep = "" 
+    print(lower_string.closed)
 
+    #remove "stop words" via example 2"programcreek.com and geeksforgeeks.org"
+def clean_text(text):
+    with open("praise_song_for_the_day.txt") as praise_song_for_the_day:
+    STOP_WORDS = stopwords.words('english')
+    text = clean_string (text)
+    text = [word for word in text if word not in stop_words]
+    print(STOP_WORDS.closed)
 
-
+    #keep a count of how often each word is used
+def print_word_freq(text):
 
 if __name__ == "__main__":
     import argparse
