b64f27551f770d1b9e0ec0a2ff171f242a9a3e4f
Jordyn Price <88625349+JordynAntonia@users.noreply.github.com>
2021-10-28 11:33:46 -0400

    finished

--- word_frequency.py
+++ word_frequency.py
@@ -22,49 +22,53 @@ def print_word_freq(file):
             line = line.replace("'","")
             line = line.lower()
             print(line)
-            
+                lower_string = string.lower()
+                text = text.lower()
+                all_letters = "abcdefghijklmnopqrstuvwxyz"
+                text_to_keep = "" 
+    
             
 
     #normalize ALL words within each string to lowercase via "geeksforgeeks.org", "docs.python.org","bytes.lower()"
-# def clean_text(file):
-    # with open(file) as text:
-    #     lines = text.readlines()
-    #     print(b"{len(lines)} lines in the file.")
-    #     lower_string = string.lower()
-    #     text = text.lower()
-    #     all_letters = "abcdefghijklmnopqrstuvwxyz"
-        # text_to_keep = "" 
+def clean_text(file):
+    with open(file) as text:
+        lines = text.readlines()
+        print(b"{len(lines)} lines in the file.")
+        lower_string = string.lower()
+        text = text.lower()
+        all_letters = "abcdefghijklmnopqrstuvwxyz"
+        text_to_keep = "" 
 
 
     #remove "stop words" via example 2"programcreek.com and geeksforgeeks.org"
-# def clean_text(text):
-#     with open("file.text"):
-#         STOP_WORDS = stopwords.words('english')
-#     print(f"{len(STOP_WORDS)} text in the file.")
-#     text = clean_string (text)
-#     text = [word for word in text if word not in stop_words]
-#     print(file.closed)
+def clean_text(text):
+    with open("file.text"):
+        STOP_WORDS = stopwords.words('english')
+    print(f"{len(STOP_WORDS)} text in the file.")
+    text = clean_string (text)
+    text = [word for word in text if word not in stop_words]
+    
 
 #     #keep a count of how often each word is used: splitting the words up in the text file, "item in data", if else statement, creating min-max spaces 0-string2,  
 
 # def print_word_freq(text):
 #     """Read in `file` and print out the frequency of words in that file."""
 #     with open("file.txt"):
-#     str = str.split()
-#     str2 = []
-#     for i in str:
-#         if not in str2:
-#             #"append"adds value 
-#             str2.append(i)
-#     for i in range(0, len(str2)) :
-#         #to make output look like the project 
-#         print(str2[i], '|', str.count(str2[i])
-# def main ():
-#     str = STOP_WORDS
-#     freq(str)
+    str = str.split()
+    str2 = []
+    for i in str:
+        if not in str2:
+            #"append"adds value 
+            str2.append(i)
+    for i in range(0, len(str2)) :
+        #to make output look like the project 
+        print(str2[i], '|', str.count(str2[i])
+def main ():
+    str = STOP_WORDS
+    freq(str)
 
-# def _name_ =="_main"
-#     main()
+def _name_ =="_main"
+    main()
 
 #     print(file.closed)
 if __name__ == "__main__":
